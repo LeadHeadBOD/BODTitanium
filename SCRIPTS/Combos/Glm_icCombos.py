43e86546a4d911d7405d41dac9dedefd14d4a20a
LeadHeadBOD <97219713+LeadHeadBOD@users.noreply.github.com>
2022-01-30 06:23:08 +0200

    Add files via upload

---
+++ SCRIPTS/Combos/Glm_icCombos.py
@@ -0,0 +1,16 @@
+######################################################
+#
+# Create sets of attacks
+#
+#        - GLM -
+#
+######################################################
+
+
+import Bladex
+
+###############
+# Ice Golem #
+###############
+import GlmCombos
+GlmCombos.AddGolemAttacks(Bladex.GetCharType("Golem_ice","Glm"), "Glm_ic")
