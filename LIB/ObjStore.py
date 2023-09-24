299eab98b19754d329c3750e616fc5f98784090a
LeadHead <97219713+LeadHeadBOD@users.noreply.github.com>
2023-09-24 17:25:01 +0300

    Fixed re-release memory leak
    https://github.com/LeadHeadBOD/BODTitanium/issues/101

---
+++ LIB/ObjStore.py
@@ -0,0 +1,52 @@
+##///
+##||| OBJSTORE.PY TITANIUM
+##||| Change list:
+##||| * Fixed re-release specific memory leak (thanks, Tomash!)
+##||| * 
+##\\\ 
+
+import sys
+
+
+
+
+ObjectsStore={}
+
+StoreIndex=0 # Que no se me olvide guardar este valor al guardar partida
+
+
+def GetNewId():
+    global StoreIndex
+
+    StoreIndex=StoreIndex+1
+    key=str(StoreIndex)
+    while ObjectsStore.has_key(key):
+        StoreIndex=StoreIndex+1
+        key=str(StoreIndex)
+
+##    print "GetNewId()",key
+    return key
+
+
+def CheckStore():
+
+##    # Recoleccion de basura
+    for i in ObjectsStore.keys():
+        if sys.getrefcount(ObjectsStore[i])<2:
+            # garbage collection, if the only reference to the class is here, delete it
+            print "CheckStore(): "+`ObjectsStore[i]`+" deleted by garbage collection" # ,rfcount # To FireFalcom, SNEG and GeneralArcade,
+            del ObjectsStore[i]                                                                  # Your "massive patchwork" introduced a memory leak, good job!
+                                                                                                 # Thanks, Tomash, for finding the cause        -LeadHead
+    # Objetos que ya no son validos
+    for i in ObjectsStore.keys():    
+        try:
+            obj=ObjectsStore[i]
+            ret=obj.persistent_check()
+            if not ret:
+                del ObjectsStore[i]
+        except AttributeError:
+            pass
+        except TypeError:
+            pass
+    
+ 
