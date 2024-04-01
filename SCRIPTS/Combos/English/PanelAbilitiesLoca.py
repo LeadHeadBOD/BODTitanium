e2ec10f172a9666df49715dc69d70d5976437227
LeadHead <97219713+LeadHeadBOD@users.noreply.github.com>
2024-04-01 18:13:12 +0300

    PanelAbilitiesLoca.py --> centered barb
    Centered titles
    Removed unnecessary hellwind space #121

--- SCRIPTS/Combos/English/PanelAbilitiesLoca.py
+++ SCRIPTS/Combos/English/PanelAbilitiesLoca.py
@@ -1,104 +1,12 @@
-##///
-##||| Combos/English/PanelAbilitiesLoca.PY TITANIUM
-##||| Change list:
-##||| * Fixed "Shredder" typo
-##||| * Ability 'titles' should be more centered now
-##||| * 
-##\\\ 
-
-############################################ ENGLISH ############################################
-abilities["English"]["Amazon_N"]["Title"] = ["Spear", "Spear"]
+abilities["English"]["Amazon_N"]["Level"] = ["L.2", "L.4"]
 abilities["English"]["Amazon_N"]["Attack"] = ["Attack: WHIRLWIND", "Attack: BACK SMASH"]
-abilities["English"]["Amazon_N"]["Level"] = ["L.2","L.4"]
-abilities["English"]["Barbarian_N"]["Title"] = [
-"""  Two-
- Handed
-Swords""",
-"""  Two-
- Handed
- Swords
-And Axes""",
-"""  Two-
- Handed
- Swords
-And Axes""",
-"""  Two-
- Handed
-Swords""",
-"""  Two-
- Handed
-Swords""",
-"""  Two-
- Handed
-Swords""",
-"""  Two-
- Handed
- Swords
-And Axes""",
-"""  Two-
- Handed
-Swords""",
-"""  Two-
-Handed
-  Axes""",
-"""  Two-
- Handed
-Swords"""]
-abilities["English"]["Barbarian_N"]["Attack"] = ["Attack: RAGE", "Attack: RIGHT DODGE SLICE", "Attack: LEFT DODGE SLICE", "Attack: BACK SMASH", "Attack: THUNDER SMASH", "Attack: THUNDER SWEEP", "Attack: SKY SMASH", "Attack: DEATH DEALER", "Attack: SHREDDER", "Attack: HELL WIND"]
-abilities["English"]["Barbarian_N"]["Level"] = ["L.2", "L.3", "L.3", "L.4", "L.6", "L.7", "L.8", "L.9", "L.11", "L.12"]
-abilities["English"]["Dwarf_N"]["Title"] = ["""    One-
-  Handed
-Hammer
- And Axe""",
-"""    One-
-  Handed
-Hammer
- And Axe""",
-"""    One-
-  Handed
-Hammer
- And Axe""",
-"""    One-
-  Handed
-Hammer
- And Axe"""]
-abilities["English"]["Dwarf_N"]["Attack"] = ["Attack: RIGHT DODGE SMASH", "Attack: LEFT DODGE SMASH", "Attack: BACK SMASH", "Attack: CYCLONE"]
+abilities["English"]["Amazon_N"]["Title"] = ["Spear", "\nSpear\n"]
 abilities["English"]["Dwarf_N"]["Level"] = ["L.2", "L.2", "L.4", "L.5"]
-abilities["English"]["Knight_N"]["Title"] = ["""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace""",
-"""    One -
-  Handed
-  Sword
-And Mace"""]
-abilities["English"]["Knight_N"]["Attack"] = ["Attack:  RIGHT DODGE ATTACK", "Attack: LEFT DODGE ATTACK", "Attack: NORTH CROSS", "Attack: BACK ATTACK", "Attack: SPEED KILL", "Attack: HAWK ATTACK", "Attack:  LOW SLASH", "Attack: RIPPER", "Attack: SOUTH CROSS"]
+abilities["English"]["Dwarf_N"]["Attack"] = ["Attack: RIGHT DODGE SMASH", "Attack: LEFT DODGE SMASH", "Attack: BACK SMASH", "Attack: CYCLONE"]
+abilities["English"]["Dwarf_N"]["Title"] = ["One-\nHanded\nHammer\nAnd Axe", "\nOne-\nHanded\nHammer\nAnd Axe", "\nOne-\nHanded\nHammer\nAnd Axe", "\nOne-\nHanded\nHammer\nAnd Axe\n"]
+abilities["English"]["Barbarian_N"]["Level"] = ["L.2", "L.3", "L.3", "L.4", "L.6", "L.7", "L.8", "L.9", "L.11", "L.12"]
+abilities["English"]["Barbarian_N"]["Attack"] = ["Attack: RAGE", "Attack: RIGHT DODGE SLICE", "Attack: LEFT DODGE SLICE", "Attack: BACK SMASH", "Attack: THUNDER SMASH", "Attack: THUNDER SWEEP", "Attack: SKY SMASH", "Attack: DEATH DEALER", "Attack: SHREDDERD", "Attack: HELL WIND"]
+abilities["English"]["Barbarian_N"]["Title"] = ["Two-\nHanded\nSwords", "\nTwo-\nHanded\nSwords\nAnd Axes", "\nTwo-\nHanded\nSwords\nAnd Axes", "\nTwo-\nHanded\nSwords", "\nTwo-\nHanded\nSwords", "\nTwo-\nHanded\nSwords", "\nTwo-\nHanded\nSwords\nAnd Axes", "\nTwo-\nHanded\nSwords", "\nTwo-\nHanded\nAxes", "\nTwo-\nHanded\nSwords\n"]
 abilities["English"]["Knight_N"]["Level"] = ["L.2", "L.2", "L.3", "L.4", "L.5", "L.6", "L.7", "L.8", "L.12"]
+abilities["English"]["Knight_N"]["Attack"] = ["Attack:  RIGHT DODGE ATTACK", "Attack: LEFT DODGE ATTACK", "Attack: NORTH CROSS", "Attack: BACK ATTACK", "Attack: SPEED KILL", "Attack: HAWK ATTACK", "Attack:  LOW SLASH", "Attack: RIPPER", "Attack: SOUTH CROSS"]
+abilities["English"]["Knight_N"]["Title"] = ["One -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace", "\nOne -\nHanded\nSword\nAnd Mace\n"]
