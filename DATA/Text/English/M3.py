a51f0cf3038b6187a3cf0e0d60fc2531526acfc0
LeadHead <97219713+LeadHeadBOD@users.noreply.github.com>
2024-04-07 22:36:19 +0300

    Text/M3.py --> Added missing dwarf narration
    Added subtitles for the missing part of the English narration
    #99

---
+++ DATA/Text/English/M3.py
@@ -0,0 +1,44 @@
+
+Textos['M3T1']=(16.0,
+      255,255,255,  	
+ 	"""Khazel Zalam is one of the most important and ancient cities of the dwarves. \n"""
+	"""The wealth of its gold-seams and diamond-lodes was exhausted centuries ago, \n"""
+	"""but those times of splendour are recalled in its well-preserved and  \n"""
+	"""extensive library, and its wise men are famed among the clans. \n \n"""
+	"""In the underground passages of your home city Yotun, \n"""
+	"""strange signs and shadows have been observed. \n"""
+	"""Bands of orcs menace the surrounding area with increasing boldness, \n"""
+	"""so you've been sent by the elders to consult the Khazel Zalam scholars. \n \n"""
+	"""Similar portents have occurred in the past... \n"""
+	"""perhaps answers are to be found among their ancient manuscripts. \n"""
+      )
+
+Textos['M3T2']=(8.0,
+      255,255,255,       
+	"""\n\nBy the gods of Hell! \n"""
+	"""The city has fallen. So many dead... \n"""
+	"""How can this be?\n"""
+ 	)
+
+Textos['M3T3']=(12.0,
+      255,255,255,
+	"""The Evil that settled on Earth was destroyed\n"""
+	"""but its seed remains hidden and shall one day return.\n"""
+	"""When it does, it shall fall to us to remember ancient alliances.\n"""
+	"""The gifts of the gods and Runes of Power shall lead us to peace once more.\n"""
+      )
+      
+Textos['M3T4']=(20.0,
+      255,255,255,
+	"""The city has been attacked, and its inhabitants are gone. \n"""
+	"""The corpses of a group of valiant defenders are scattered \n"""
+	"""throughout its passages. In one room of the library you \n"""
+	"""found a book which speaks of mysterious Runes of Power, and \n"""
+	"""suggests that assistance could come from the world of Men. \n"""
+	"""Now that Khazel Zalam has fallen, all the cities of the dwarves \n"""
+	"""will be on their guard. Their gates will be barred, and woe betide \n"""
+	"""those like you who remain on the outside, for the dwarves will not \n"""
+	"""emerge until the situation has been resolved. The only solution is \n"""
+	"""to solve the whole mystery and return home as soon as possible. \n"""
+      )
+
