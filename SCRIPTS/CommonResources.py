71dae06f02337f5b027022eff48483743d33c22b
LeadHead <97219713+LeadHeadBOD@users.noreply.github.com>
2024-04-21 16:50:21 +0300

    CommonResources.py --> Add missing Armeropieza tex
    Armeropieza texture will now get loaded correctly irrespective of map. #130

--- SCRIPTS/CommonResources.py
+++ SCRIPTS/CommonResources.py
@@ -2,8 +2,7 @@
 ##||| CommonResources.PY TITANIUM
 ##||| Change list:
 ##||| * pocimas_e.mmp to make sure empty potions work
-##||| * Added generic texture for armero-pieza, 
-##|||   alternative was to modify the material in the respective models, I can't be bothered.
+##||| * Fixed armero missing piece textures. No idea why they are in 3dChars folder.
 ##\\\ 
 
 
@@ -18,7 +17,7 @@ def Init():
     BBLib.ReadMMP('../../3dobjs/weapons.mmp')
     BBLib.ReadMMP('../../3dobjs/genericos.mmp')
     BBLib.ReadMMP('../../3dobjs/pocimas_e.mmp') # Added -LeadHead
-    BBLib.ReadMMP('../../3dobjs/armeropie.mmp') # generic texture for armero pieza -LeadHead
+    BBLIB.ReadMMP('../../3dChars/roturas.mmp')  # Added. Contains missing armero piece texture, etc. -LeadHead
 
     
     Bladex.BodInspector()
