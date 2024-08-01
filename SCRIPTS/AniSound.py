ea00bfbfc06887aa670ef1240ca14d0ea4518657
LeadHead <97219713+LeadHeadBOD@users.noreply.github.com>
2024-08-01 21:37:05 +0300

    AniSound.py --> Added comments

--- SCRIPTS/AniSound.py
+++ SCRIPTS/AniSound.py
@@ -1,4 +1,12 @@
-
+##///
+##||| ANISOUND.PY TITANIUM
+##||| Change list:
+##||| * Added Ice Golem
+##||| * Added Dark Ork
+##||| * Added Gold Ork
+##||| * PLAGUE: Generic sound effects like "draw weapon", "hit sounds" and others should be under a singular AniSound module
+##|||    so the sounds don't get imported multiple times under different varriable names
+##\\\ 
 
 
 
@@ -28,10 +36,10 @@ from AniSoundDkn import *
 from AniSoundZkn import *
 from AniSoundGdm import *
 from AniSoundAnk import *
-from AniSoundDok import *
-from AniSoundGok import *
+from AniSoundDok import * #Added
+from AniSoundGok import * #Added
 from AniSoundGlm_st import *
 from AniSoundGlm_cl import *
 from AniSoundGlm_lv import *
 from AniSoundGlm_mt import *
-from AniSoundGlm_ic import *
+from AniSoundGlm_ic import * #added
